99cdb29a0696a50659224529022171d4e36a6368
smhasanmansur <smhasanmansur@gmail.com>
2016-03-02 17:43:14 +0600

    proto file attached

--- proto.js
+++ proto.js
@@ -1,17 +1,14 @@
 console.log('#################### Prototype Property ##################');
+
+/*
+Every JavaScript function has a prototype property (this property is empty by default), and you attach properties and methods on this prototype property when you want to implement inheritance. 
+
+Every object inherits properties from some other object, and it is this other object that is the object’s prototype attribute or “parent.” (You can think of the prototype attribute as the lineage or the parent).
+*/
 function employee (name, id) {
     this.name = name;
     this.id = id;
-    
 }
-
-var sharif  = new employee("sharif", "1");
-var shahadat = new employee("shahadat", "2");
-
-employee.prototype.company = "synchronous";
-console.log(employee.prototype);
-console.log("company" in sharif);
-console.log(employee.constructor);
 
 
 
