37f7e5b9b28d8e7b7afe011b7cb14c7ea8e31a6f
smhasanmansur <smhasanmansur@gmail.com>
2016-03-12 17:55:34 +0600

    global scope

--- scope.js
+++ scope.js
@@ -15,7 +15,7 @@ console.log("name is from global scope:" + name);
 showName();
 
 /*
-If you don't declare your local variables with the var keyword, they are part of the global scope​
+If we don't declare our local variables with the var keyword, they are part of the global scope​
 */
 console.log(":::::::::::::::::::::::::::::::::::::");
 var fruit = "banana";
@@ -35,3 +35,39 @@ console.log("global fruit is now changed:" + showOrdinaryFruit());
 
 console.log(":::::::::::::::::::::::::::::::::::::");
 
+
+/*
+If we declare a global variable and a local variable with the same name, the local variable will have priority when we attempt to use the variable inside a function
+*/
+
+var user = "hasan";
+
+function showUser () {
+    var user = "mansur";
+    console.log("user from local scope:" + user);
+}
+
+showUser();
+
+console.log("--------------------------------------Global Scope-----------------------------");
+
+function showMyName1 () {
+    myName = "hasan";
+    console.log(myName);
+}
+
+function showMyName2 () {
+    console.log(myName);
+}
+showMyName1();
+showMyName2();
+
+/*
+If a variable is initialized (assigned a value) without first being declared with the var keyword, it is automatically added to the global context and it is thus a global variable
+*/
+function showAge () {
+    age = "99";
+    console.log(age);
+}
+showAge();
+console.log(age);
