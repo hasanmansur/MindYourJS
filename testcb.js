02b9005fbe9cc8d5cba3566c90b42e2a63f9fee9
smhasanmansur <smhasanmansur@gmail.com>
2016-04-06 01:48:19 +0600

    testcb.js edited with comments

--- testcb.js
+++ testcb.js
@@ -1,3 +1,10 @@
+/*
+A callback function, also known as a higher-order function, is a function that is passed to another function (let’s call this other function “otherFunction”) as a parameter, and the callback function is called (or executed) inside the otherFunction.
+
+When we pass a callback function as an argument to another function, the callback is executed at some point inside the containing function’s body just as if the callback were defined in the containing function. This means the callback is a closure. 
+As we know, closures have access to the containing function’s scope, so the callback function can access the containing functions’ variables, and even the variables from the global scope.
+*/
+
 function childFunction (x, y, z) {
     console.log("hello from childFunction");
     return x + y + ":" + z;
