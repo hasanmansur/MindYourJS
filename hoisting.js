94f41fe758554a53b2f245d83cdf6c1299a31e72
Shahadat Hossain <shahadat.cseru@gmail.com>
2016-07-26 14:00:30 +0600

    how javascript actually interprets variable declaration

--- hoisting.js
+++ hoisting.js
@@ -1,3 +1,5 @@
+'use strict';
+
 console.log("################################### Variable Hoisting #############################");
 
 /*
@@ -14,15 +16,42 @@ function showName () {
 }
 showName();
 
+console.log("::::::::::::::::::::::::::::::::::::::::");
+
+var name = "Shahadat";
+
+(function(){
+	console.log(name); // Shahadat
+	name = "Hossain";
+	console.log(name); // Hossain
+})();
+
+console.log("::::::::::::::::::::::::::::::::::::::::");
+/***
+		name will be undefined. because local variable declaration is hoisted here,
+		javascript interpret this code as it is like below:
+		var name;
+		console.log(name);
+		name = "Hossain";
+		console.log(name);
+***/
+(function(){
+	console.log(name); // undefined 
+	var name = "Hossain";
+	console.log(name); // Hossain
+})();
+
+
 /*
 function declaration takes precedence over variable declarations (but not over variable assignment).
 variable assignment is not hoisted, and neither is function assignment.
 */
 console.log("################################### Function Hoisting #############################");
-var myName;
+
 function myName () {
     console.log ("hasan");
 }
+var myName;
 console.log(typeof myName); // function
 
 console.log("::::::::::::::::::::::::::::::::::::::::");
@@ -31,5 +60,3 @@ function myName () {
     console.log ("hasan");
 }
 console.log(typeof myName); // string
-
-
